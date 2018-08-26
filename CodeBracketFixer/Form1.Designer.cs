c594f3ae9fc0437913843bec01393818977e9c04
LupusUmbrarum <lupus.umbrarum.9@gmail.com>
2018-08-26 14:08:12 -0500

    Initial Commit
    Just committing what I'd previously done.

---
+++ CodeBracketFixer/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace CodeBracketFixer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fixButton = new System.Windows.Forms.Button();
+            this.screwButton = new System.Windows.Forms.Button();
+            this.copyButton = new System.Windows.Forms.Button();
+            this.pasteButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // fixButton
+            // 
+            this.fixButton.AutoSize = true;
+            this.fixButton.Location = new System.Drawing.Point(13, 13);
+            this.fixButton.Name = "fixButton";
+            this.fixButton.Size = new System.Drawing.Size(75, 23);
+            this.fixButton.TabIndex = 0;
+            this.fixButton.Text = "Fix code";
+            this.fixButton.UseVisualStyleBackColor = true;
+            this.fixButton.Click += new System.EventHandler(this.fixButton_Click);
+            // 
+            // screwButton
+            // 
+            this.screwButton.Location = new System.Drawing.Point(94, 13);
+            this.screwButton.Name = "screwButton";
+            this.screwButton.Size = new System.Drawing.Size(88, 23);
+            this.screwButton.TabIndex = 1;
+            this.screwButton.Text = "Screw up code";
+            this.screwButton.UseVisualStyleBackColor = true;
+            this.screwButton.Click += new System.EventHandler(this.screwButton_Click);
+            // 
+            // copyButton
+            // 
+            this.copyButton.Location = new System.Drawing.Point(188, 13);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(75, 23);
+            this.copyButton.TabIndex = 2;
+            this.copyButton.Text = "Copy";
+            this.copyButton.UseVisualStyleBackColor = true;
+            this.copyButton.Click += new System.EventHandler(this.copyButton_Click);
+            // 
+            // pasteButton
+            // 
+            this.pasteButton.Location = new System.Drawing.Point(269, 13);
+            this.pasteButton.Name = "pasteButton";
+            this.pasteButton.Size = new System.Drawing.Size(75, 23);
+            this.pasteButton.TabIndex = 3;
+            this.pasteButton.Text = "Paste";
+            this.pasteButton.UseVisualStyleBackColor = true;
+            this.pasteButton.Click += new System.EventHandler(this.pasteButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(13, 43);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(759, 506);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pasteButton);
+            this.Controls.Add(this.copyButton);
+            this.Controls.Add(this.screwButton);
+            this.Controls.Add(this.fixButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Code Bracket Fixer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button fixButton;
+        private System.Windows.Forms.Button screwButton;
+        private System.Windows.Forms.Button copyButton;
+        private System.Windows.Forms.Button pasteButton;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ CodeBracketFixer/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeBracketFixer
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Used to temporarily store changes to the text in textBox1
+        /// </summary>
+        private List<string> newLines;
+
+        /// <summary>
+        /// Used to find the appropriate index while fixing/screwing up code
+        /// </summary>
+        int count;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Searches a string for the last empty space, then returns the appropriate indentation as a string
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        private string getIndentationSpace(string str)
+        {
+            string amt = "";
+
+            for (int x = 0; x < str.Length; x++)
+            {
+                if (str[x] == ' ')
+                {
+                    amt += " ";
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return amt;
+        }
+
+        /// <summary>
+        /// Finds all lines of code that end with an open curly bracket '{', and moves the curly bracket down one line
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void fixButton_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < textBox1.Lines.Length; x++)
+            {
+                try
+                {
+                    if (textBox1.Lines[x].Last() == '{' && !String.IsNullOrWhiteSpace(textBox1.Lines[x].Substring(0, textBox1.Lines[x].Length - 1)))
+                    {
+                        newLines = new List<string>();
+
+                        count = 0;
+
+                        while (count < x)
+                        {
+                            newLines.Add(textBox1.Lines[count++]);
+                        }
+
+                        newLines.Add(textBox1.Lines[count].Substring(0, textBox1.Lines[count].Length - 1));
+
+                        newLines.Add(getIndentationSpace(textBox1.Lines[count]) + "{");
+
+                        while (++count < textBox1.Lines.Length)
+                        {
+                            newLines.Add(textBox1.Lines[count]);
+                        }
+                        textBox1.Lines = newLines.ToArray();
+                    }
+                }
+                catch (Exception ex) { }
+            }
+            newLines = null;
+            GC.Collect();
+        }
+
+        /// <summary>
+        /// Finds all lines that contain only open curly brackets '{', and puts the curly bracket at the end of the line above it
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void screwButton_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < textBox1.Lines.Length; x++)
+            {
+                try
+                {
+                    if (textBox1.Lines[x].Last() == '{' && String.IsNullOrWhiteSpace(textBox1.Lines[x].Substring(0, textBox1.Lines[x].Length - 1)))
+                    {
+                        newLines = new List<string>();
+
+                        int count = 0;
+
+                        while (count < x)
+                        {
+                            newLines.Add(textBox1.Lines[count++]);
+                        }
+
+                        newLines[count - 1] += "{";
+
+                        while (++count < textBox1.Lines.Length)
+                        {
+                            newLines.Add(textBox1.Lines[count]);
+                        }
+
+                        textBox1.Lines = newLines.ToArray();
+                    }
+                }
+                catch (Exception ex) { }
+            }
+            newLines = null;
+            GC.Collect();
+        }
+
+        /// <summary>
+        /// Attempts to paste the contents of the clipboard to textBox1
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void copyButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(textBox1.Text);
+            }
+            catch (Exception ex) { }
+        }
+
+        /// <summary>
+        /// Attempts to copy the contents of textBox1 to the clipboard
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pasteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox1.Text = Clipboard.GetText();
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
